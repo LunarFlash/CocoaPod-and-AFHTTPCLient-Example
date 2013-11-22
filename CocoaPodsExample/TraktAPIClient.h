04efa6dd79e4c62b44db1f38cf54ef09ed75ea90
Yi <logins.yiwang@gmail.com>
2013-11-22 16:21:44 -0500

    first commit

---
+++ CocoaPodsExample/TraktAPIClient.h
@@ -0,0 +1,13 @@
+//
+//  TracktAPIClient.h
+//  CocoaPodsExample
+//
+//  Created by Terry Wang on 11/22/13.
+//  Copyright (c) 2013 ASHP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TracktAPIClient : NSObject
+
+@end
---
+++ CocoaPodsExample/TraktAPIClient.m
@@ -0,0 +1,13 @@
+//
+//  TracktAPIClient.m
+//  CocoaPodsExample
+//
+//  Created by Terry Wang on 11/22/13.
+//  Copyright (c) 2013 ASHP. All rights reserved.
+//
+
+#import "TracktAPIClient.h"
+
+@implementation TracktAPIClient
+
+@end
